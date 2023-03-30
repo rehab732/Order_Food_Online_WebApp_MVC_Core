6b917c1f236efa3fcb491badf4e08f77f500e68e
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-31 00:37:28 +0200

    testsomthing

--- Order_Food_Online/Order_Food_Online/Areas/Resturant/Controllers/ResturantsController.cs
+++ Order_Food_Online/Order_Food_Online/Areas/Resturant/Controllers/ResturantsController.cs
@@ -53,9 +53,7 @@ namespace Order_Food_Online.Areas.Resturant.Controllers
             return View();
         }
 
-        // POST: Resturant/Resturants/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Route("Resturant/Resturants/Create")]
