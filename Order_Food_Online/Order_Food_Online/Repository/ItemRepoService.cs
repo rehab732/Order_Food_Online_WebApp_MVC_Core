7dc39b09d32f3910d66c6913871dc7f1c64f8844
HassanAbdelbaset <hassanelfalt60@gmail.com>
2023-03-30 21:53:14 +0200

    - make Repository for Resturant Area

---
+++ Order_Food_Online/Order_Food_Online/Repository/ItemRepoService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order_Food_Online.Areas.Resturant.Models;
+using Order_Food_Online.Data;
+
+namespace Order_Food_Online.Repository
+{
+    public class ItemRepoService : ICRUDRepository<Items>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemRepoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(Items item)
+        {
+            _context.Remove(item);
+            _context.SaveChangesAsync();
+        }
+
+        public List<Items> GetAll()
+        {
+            return _context.Items.Include(i => i.Resturants).ToList();
+        }
+
+        public Items GetDetails(int id)
+        {
+            if (_context.Items == null)
+            {
+                return null;
+            }
+
+            var items = _context.Items
+                .Include(i => i.Resturants)
+                .FirstOrDefault(m => m.ItemsId == id);
+
+            return items;
+        }
+
+        public void Insert(Items item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChangesAsync();
+        }
+
+        public void Update(int id, Items updatedItem)
+        {
+            var item = _context.Items.Find(id);
+            item.ItemName = updatedItem.ItemName;
+            item.ImageUrl = updatedItem.ImageUrl;
+            item.Price = updatedItem.Price;
+
+            _context.SaveChangesAsync();
+        }
+    }
+}
