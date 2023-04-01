a41b860e73e5dbc6bc136543f84f268de8fc4a34
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-04-01 04:34:44 +0200

    AddLoginandEditProfileAndSeedingRoles

---
+++ Order_Food_Online/Order_Food_Online/Migrations/20230401020452_seedingroles.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order_Food_Online.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingroles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table:"Roles",
+                columns: new[] {"Id", "Name","NormalizedName", "ConcurrencyStamp" },
+                values: new object[] {Guid.NewGuid().ToString(),"User","User".ToUpper(),Guid.NewGuid().ToString()},
+                schema: "security"
+
+                );
+            migrationBuilder.InsertData(
+            table: "Roles",
+            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+            values: new object[] { Guid.NewGuid().ToString(), "Admin", "Admin".ToUpper(), Guid.NewGuid().ToString() },
+            schema: "security"
+
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [security].[Roles]");
+
+        }
+    }
+}
